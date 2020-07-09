c54429c297cdcdb052544f3ed5bbbe024df8217a
Rodrigo Moura <rodrigojorgemoura@gmail.com>
2020-07-09 17:30:50 +0100

    Iteration 2

--- main.js
+++ main.js
@@ -25,31 +25,31 @@ function drawGrid() {
   }
 }
 
-// class Character {
-//   constructor(col, row) {
-//     this.col = col;
-//     this.row = row;
-//   }
-//   moveUp() {
-//     this.row--;
-//   }
-//   moveRight() {
-//     this.col++;
-//   }
-//   moveDown() {
-//     this.row++;
-//   }
-//   move() {
-//     this.col--;
-//   }
-// }
+class Character {
+  constructor(col, row) {
+    this.col = col;
+    this.row = row;
+  }
+  moveUp() {
+    this.row--;
+  }
+  moveRight() {
+    this.col++;
+  }
+  moveDown() {
+    this.row++;
+  }
+  move() {
+    this.col--;
+  }
+}
 
-// const player = new Character(0, 0); // (0,0) = Initial position
-// console.log('player: ', player);
-// player.moveDown(); // Increase by 1 the value of player.row
-// player.moveDown(); // Increase by 1 the value of player.row
-// player.moveRight(); // Increase by 1 the value of player.col
-// console.log(player.col, player.row);
+const player = new Character(0, 0); // (0,0) = Initial position
+console.log('player: ', player);
+player.moveDown(); // Increase by 1 the value of player.row
+player.moveDown(); // Increase by 1 the value of player.row
+player.moveRight(); // Increase by 1 the value of player.col
+console.log(player.col, player.row);
 
 function drawEverything() {
   drawGrid();
